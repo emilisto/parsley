e894a7853c754e22ba62ddb59e3d2d94668a9e1c
Emil Stenqvist <emsten@gmail.com>
2013-04-26 15:19:50 +0200

    First test running, yay

--- test/core.js
+++ test/core.js
@@ -1,17 +1,33 @@
-var parsley = require('..')
+var Parsley = require('..')
   , assert = require('assert')
 ;
 
 module.exports = {
 
-  'one case': function() {
-    assert(1+1 === 2);
+  'running a function manually and as a task yields same result': function() {
+
+    var sampleFn = function(val) { return val * 2; };
+    var sampleTask = new Parsley.Task(sampleFn);
+
+    assert.equal(
+      new Parsley.Action(sampleTask, 123).run(),
+      sampleFn(123)
+    );
   },
 
-  'bad case': function() {
-    assert(false);
-  },
+  'arguments can be added to actions': function() {
 
+    var args = null
+    var sampleFn = function() {
+      args = [].concat(arguments);
+    };
 
+    var sampleTask = new Parsley.Task(sampleFn);
+    var action = new Parsley.Action(sampleTask, 'first');
+    action.addArguments('second');
+    action.run();
+
+    assert(args.length === 2)
+  }
 
 };
