fbbe743031e1f7dc304bc6eb714da0b8bd23bc97
Emil Stenqvist <emsten@gmail.com>
2013-04-26 21:35:08 +0200

    Started on the queueing part

--- tmp/amqp-producer.js
+++ tmp/amqp-producer.js
@@ -4,9 +4,12 @@ var connection = amqp.createConnection({ host: '192.168.33.14' });
 
 // Wait for connection to become established.
 connection.on('ready', function () {
+
   // Use the default 'amq.topic' exchange
 
-  var e = connection.exchange('node-flow-fanout', {type: 'fanout'});
+  var e = connection.exchange('node-flow-fanout', {
+    type: 'fanout'
+  });
 
   connection.queue('my-queue', function(q){
     q.bind(e, "*");
