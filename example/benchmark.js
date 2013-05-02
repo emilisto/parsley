8864bd34633dd9e82bb3e55f97cc4e839af22a26
Emil Stenqvist <emsten@gmail.com>
2013-05-02 08:12:03 +0200

    Added benchmark - 8 ms to from dispatch to result on localhost, yaaay!

---
+++ example/benchmark.js
@@ -0,0 +1,19 @@
+var Parsley = require('..')
+;
+
+var parsley = new Parsley();
+console.log(parsley.Task);
+process.exit();
+
+Parsley.config.redis = { host: 'localhost', port: 16379 };
+
+var command = new Parsley.Command(function(arg) {
+  return "I'm from the task";
+}, 'and then some', 123, 'arguments');
+
+var startTime = +new Date;
+command.dispatch().get(function() {
+  var delta = +new Date - startTime;
+  console.log('Runtime: %d ms', delta);
+  process.exit();
+});
