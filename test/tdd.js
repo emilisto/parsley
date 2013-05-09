83788ba111bcaaffa97a108125ec1c707f9d3ecd
Emil Stenqvist <emsten@gmail.com>
2013-05-09 13:10:38 +0200

    [testing] Covered all the stuff I've done so far with asynchronous mocha tests. Unfortunately, currently a Parsley worker has to be run separately. This should be remedied eventually - so that the worker can be run in the same process that dispatches the commands.

---
+++ test/tdd.js
@@ -0,0 +1,46 @@
+var Parsley = require('..')
+var mocha = require('mocha');
+
+Parsley.config.redis = { port: 16379 };
+
+//describe('Parsley.Canvas', function() {
+
+  //var worker;
+
+  //before(function() { worker = new Parsley.Worker(); });
+  //beforeEach(function() { worker.start(); });
+  //afterEach(function() { worker.end(); });
+
+  
+
+  ////it('should take less than 500ms', function(done){
+    ////this.timeout(500);
+    ////setTimeout(done, 300);
+  ////})
+
+  ////it('should take less than 500ms as well', function(done){
+    ////this.timeout(500);
+    ////setTimeout(done, 200);
+  ////})
+
+//})
+
+//suite('Parsley.Canvas', function(){
+  //setup(function(){
+    //console.log('setup');
+    //// ...
+  //});
+
+  //before(function() { worker = new Parsley.Worker(); });
+  //beforeEach(function() { worker.start(); });
+  //afterEach(function() { worker.end(); });
+
+
+
+  //suite('#indexOf()', function(){
+    //test('should return -1 when not present', function(){
+      //assert.equal(-1, [1,2,3].indexOf(4));
+    //});
+  //});
+//});
+
