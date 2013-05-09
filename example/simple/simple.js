a8c4fc7c94059c589826344c708de2dbe6b7de1a
Emil Stenqvist <emsten@gmail.com>
2013-05-09 23:01:46 +0200

    [task] Added so the function names of commands are actually used in the logging

--- example/simple/simple.js
+++ example/simple/simple.js
@@ -2,7 +2,7 @@ var Parsley = require('../..');
 
 Parsley.config.redis = { port: 16379 };
 
-new Parsley.Command(function() {
+new Parsley.Command(function simple() {
   return 'Im some kind of result';
 }).dispatch() .get(function(err, result) {
   console.log('task result:', result);
