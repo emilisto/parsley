071f6a4826c18385e225182a1fda1b434714149c
Emil Stenqvist <emsten@gmail.com>
2013-05-09 23:43:59 +0200

    Got the damn Chord to work, yaaaaay

--- example/simple/chord.js
+++ example/simple/chord.js
@@ -6,14 +6,14 @@ Parsley.config.redis = {
 };
 
 var makeCommand = function(i) {
-  return new Parsley.Command(function(i) {
+  return new Parsley.Command(function actualCommand(i) {
     var n = Math.random() * 10e7;
     for(var j = 0; j < n; j++);
     return "return from task " + i + "random number" + new String(n | 0);
   }, i);
 };
 
-var commands = _(1).times(makeCommand);
+var commands = _(10).times(makeCommand);
 
 new Parsley.Canvas.Chord(commands)
   .dispatch()
