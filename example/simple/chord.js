41074cb21daa06f0216f99964a9eb6eebe736980
Emil Stenqvist <emsten@gmail.com>
2013-05-10 13:20:53 +0200

    [logging] Improved logging by removing useless debug logs, and only outputting first part of id's.

--- example/simple/chord.js
+++ example/simple/chord.js
@@ -6,14 +6,14 @@ Parsley.config.redis = {
 };
 
 var makeCommand = function(i) {
-  return new Parsley.Command(function actualCommand(i) {
+  return new Parsley.Command(function slowTest(i) {
     var n = Math.random() * 10e7;
     for(var j = 0; j < n; j++);
     return "return from task " + i + "random number" + new String(n | 0);
   }, i);
 };
 
-var commands = _(10).times(makeCommand);
+var commands = _(4).times(makeCommand);
 
 new Parsley.Canvas.Chord(commands)
   .dispatch()
